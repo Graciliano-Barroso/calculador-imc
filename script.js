80157aef9447ba94db98192d3821af15d16db590
Rico <grico484@gmail.com>
2022-10-26 13:16:31 -0400

    feat: Ceated Event click

---
+++ script.js
@@ -0,0 +1,8 @@
+let buttonCalcular = document.querySelector('#calcular');
+
+buttonCalcular.addEventListener('click', ()=> {
+    let nome = document.querySelector('#nome');
+    let altura = document.querySelector('#altura');
+    let peso = document.querySelector('#peso');
+    let resultado = document.querySelector('#resultado');
+});
