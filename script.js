5f127c366bf6a3de78da7a71a39db8027ee7a8b0
Rico <grico484@gmail.com>
2022-10-26 14:27:23 -0400

    feat: Validation if all fields are empty

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let buttonCalcular = document.querySelector('#calcular');
 
 buttonCalcular.addEventListener('click', ()=> {
-    let nome = document.querySelector('#nome');
-    let altura = document.querySelector('#altura');
-    let peso = document.querySelector('#peso');
+    let nome = document.querySelector('#nome').value;
+    let altura = document.querySelector('#altura').value;
+    let peso = document.querySelector('#peso').value;
     let resultado = document.querySelector('#resultado');
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+
+    }else {
+        resultado.textContent = 'Preencha todos os campos';
+    }
 });
